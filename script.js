bfeb81703144146ff62f7b68277aeafa7be6cc56
Cal-theDevGirl <120177364+Cal-theDevGirl@users.noreply.github.com>
2022-12-09 11:41:47 +0000

    Update script.js
    added code for the script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+// Get the menu icon
+const menuIcon = document.querySelector('.menu-icon');
 
+// Add an event listener to the menu icon that toggles the display of the nav items when the menu icon is clicked
+menuIcon.addEventListener('click', () => {
+  document.querySelector('nav > ul').classList.toggle('show');
+});
